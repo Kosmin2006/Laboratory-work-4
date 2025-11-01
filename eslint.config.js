77418592f3a578f8cfe58a19a3de84883c75be54
Kosmin2006 <162718279+Kosmin2006@users.noreply.github.com>
2025-11-01 17:02:02 +0200

    Update eslint.config.js

--- eslint.config.js
+++ eslint.config.js
@@ -2,9 +2,12 @@ export default [
   {
     files: ["**/*.js"],
     languageOptions: {
-      ecmaVersion: 2021,
-      sourceType: "module",
-    },
+  ecmaVersion: 2021,
+  sourceType: "module",
+  globals: {
+    console: "readonly",
+  },
+},
     rules: {
       semi: "error",          // вимагає крапки з комою
       "no-undef": "error",    // забороняє невизначені змінні
