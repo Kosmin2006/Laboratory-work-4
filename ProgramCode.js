5ded8a85d203a82fee66656f476a3a29d3ccd54e
Kosmin2006 <162718279+Kosmin2006@users.noreply.github.com>
2025-11-01 16:46:54 +0200

    Update ProgramCode.js

--- ProgramCode.js
+++ ProgramCode.js
@@ -1,17 +1,3 @@
-export default [
-  {
-    files: ["**/*.js"],
-    languageOptions: {
-      ecmaVersion: "latest",
-      sourceType: "module",
-    },
-    rules: {
-      semi: ["error", "always"],
-      "no-unused-vars": "error",
-      "no-undef": "error",
-    },
-  },
-];
 function main() {
   const productName = "MyLearningSystem";
   console.log(`Назва програмного продукту: ${productName}`);
